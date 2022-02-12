1ceea8283e5fb46011bab1467a63730fb875f76a
sduque <samuduke00@gmail.com>
2022-02-12 02:32:32 -0500

    commit 3, se agregaron nuevas funcionalidades para el movimiento de las barras

--- pong/main.js
+++ pong/main.js
@@ -77,7 +77,8 @@ self.addEventListener("load", main);
 function main() {
     var board = new Board(800, 400);
     var bar = new Bar(20, 100, 40, 100, board);
-    var canvas = document.getElementById('canvas')
+    var bar = new Bar(735, 100, 40, 100, board);
+    var canvas = document.getElementById('canvas');
     var board_view = new BoardView(canvas, board);
     board_view.draw();
 }
