2002f042756b63e88d31b378b5e3f31064dcae0d
sduque <samuduke00@gmail.com>
2022-02-12 19:33:50 -0500

    commit 6: se crea la funcion de la colision de la pelota

--- pong/main.js
+++ pong/main.js
@@ -8,7 +8,7 @@
         this.ball = null;
         this.playing = false;
     }
-    self.Board.prototype = {
+    self.Board.prototype = { // metodo que retorla la pelota y las barras
         get elements() {
             var elements = this.bars.map(function(bar) { return bar; });
             elements.push(this.ball);
@@ -25,6 +25,9 @@
         this.speed_x = 3;
         this.board = board;
         this.direction = 1;
+        this.bounce_angle = 0;
+        this.max_bounce_angle = Math.PI / 12;
+        this.speed = 3;
 
         board.ball = this;
         this.kind = "circle";
@@ -35,6 +38,26 @@
         move: function() {
             this.x += (this.speed_x * this.direction);
             this.y += (this.speed_y);
+        },
+        get width() {
+            return this.radius * 2;
+        },
+        get height() {
+            return this.radius * 2;
+        },
+        collision: function(bar) {
+            //reacciona a la colision con una barra que recibe como parametro
+            var relative_intersect_y = (bar.y + (bar.height / 2)) - this.y;
+
+            var normalized_intersect_y = relative_intersect_y / (bar.height / 2);
+
+            this.bounce_angle = normalized_intersect_y * this.max_bounce_angle;
+
+            this.speed_y = this.speed * -Math.sin(this.bounce_angle);
+            this.speed_x = this.speed * Math.con(this.bounce_angle);
+
+            if (this.x > (this.board.width / 2)) this.direction = -1;
+            else this.direction = 1;
         }
     }
 })();
@@ -65,7 +88,7 @@
 
 (function() {
     self.BoardView = function(canvas, board) {
-
+        // se modofician las dimeisones del canvas por las del board
         this.canvas = canvas;
         this.canvas.width = board.width;
         this.canvas.height = board.height;
@@ -86,16 +109,48 @@
             };
 
         },
+        check_collisions: function() {
+            for (var i = this.board.bars.length - 1; i >= 0; i--) {
+                var bar = this.board.bars[i];
+                if (hit(bar, this.board.ball)) {
+                    this.board.ball.collision(bar);
+                }
+            }
+        },
         play: function() {
             if (this.board.playing) {
                 this.clean();
                 this.draw();
+                this.check_collisions();
                 this.board.ball.move();
             }
         }
     }
 
-    function draw(ctx, element) {
+
+    function hit(a, b) {
+        //revisa si a colisiona con b 
+        var hit = false;
+        //colisiones horizontales
+        if (b.x + b.width >= a.x && b.x < a.x + a.width) {
+            //colisiones verticals
+            if (b.y + b.height >= a.y && b.y < a.y + a.height)
+                hit = true;
+        }
+        //colision de a con b
+        if (b.x <= a.x && b.x + b.width >= a.x + a.width) {
+            if (b.y <= a.y && b.y + b.height >= a.y + a.height)
+                hit = true;
+        }
+        //colision b con a 
+        if (a.x <= b.x && a.x + a.width >= b.x + b.width) {
+            if (a.y <= b.y && a.y + a.height >= b.y + b.height)
+                hit = true;
+        }
+        return hit;
+    }
+
+    function draw(ctx, element) { // funcion que dibuja un cuadrado
         switch (element.kind) {
             case "rectangle":
                 ctx.fillRect(element.x, element.y, element.height);
@@ -126,10 +181,10 @@ document.addEventListener("keydown", function(ev) {
         bar.down();
     }
     if (ev.keyCode === 87) {
-        //w
+        //tecla w
         bar_2.up();
     } else if (ev.keyCode === 83) {
-        //s
+        //teclas
         bar_2.down();
     } else if (ev.keyCode === 32) {
         ev.preventDefault();
@@ -138,6 +193,8 @@ document.addEventListener("keydown", function(ev) {
 
 });
 
+
+board_view.draw();
 
 window.requestAnimationFrame(controller);
 
